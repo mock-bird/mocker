98dbbb721aa4200699a685ee832ab3940a5f09b2
kklimczak <konrad.klimczak.4@gmail.com>
2018-05-21 20:45:21 +0200

    fix(models): add missing reference to path item object model

--- src/models/open-api-object.ts
+++ src/models/open-api-object.ts
@@ -1,10 +1,11 @@
 import { InfoObject } from "./info-object";
+import { PathsObject } from "./paths-object";
 
 export interface OpenAPIObject {
     openapi: string;
     info: InfoObject;
     servers?: object[];
-    paths: object;
+    paths: PathsObject;
     components?: object;
     security?: object[];
     tags?: object[];
