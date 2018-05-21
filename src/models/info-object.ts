210318cccd7b833f28a0dbb255739d4df651856a
kklimczak <konrad.klimczak.4@gmail.com>
2018-05-21 20:02:36 +0200

    feat(models): add OpenAPI and Info object models

---
+++ src/models/info-object.ts
@@ -0,0 +1,8 @@
+export interface InfoObject {
+    title: string;
+    description?: string;
+    termsOfService?: string;
+    contact?: object;
+    license?: object;
+    version: string;
+}
---
+++ src/models/open-api-object.ts
@@ -0,0 +1,13 @@
+import { InfoObject } from "./info-object";
+
+export interface OpenAPIObject {
+    openapi: string;
+    info: InfoObject;
+    servers: object[];
+    paths: object;
+    components: object;
+    security: object[];
+    tags: object[];
+    externalDocs: object;
+
+}
