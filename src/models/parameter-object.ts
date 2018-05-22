ddb575010a688f04f3a6c592be45eccb527835ec
kklimczak <konrad.klimczak.4@gmail.com>
2018-05-22 22:29:35 +0200

    feat(models): fill in parameter object model

--- src/models/parameter-object.ts
+++ src/models/parameter-object.ts
@@ -1,3 +1,5 @@
+import { SchemaObject } from "./schema-object";
+
 export interface ParameterObject {
     name: string;
     in: 'query' | 'header' | 'path' | 'cookie';
@@ -5,4 +7,11 @@ export interface ParameterObject {
     required?: boolean;
     deprecated?: boolean;
     allowEmptyValue?: boolean;
+    style?: string;
+    explode?: boolean;
+    allowReserved?: boolean;
+    schema?: SchemaObject;
+    example?: any;
+    examples?: {[example: string]: object};
+    content?: {[mediaType: string]: object};
 }
