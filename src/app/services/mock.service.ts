7985f4ec470906e15cba30e46ae714a98908f17d
kklimczak <konrad.klimczak.4@gmail.com>
2018-05-27 16:56:40 +0200

    feat(services/mock): create method to mock get response based on schema
    first proof of concept

--- src/app/services/mock.service.ts
+++ src/app/services/mock.service.ts
@@ -9,12 +9,36 @@ export class MockService {
 
     mockGetEndpoint(path: string, getOperation: OperationObject) {
         return (req: Request, res: Response) => {
-            res.json(getOperation);
+            res.json(this.prepareGetResponse(getOperation));
         }
     }
 
     parseParamsInPath(path: string): string {
         return path.replace(/{/g, ':').replace(/}/g, '');
+    }
+
+    prepareGetResponse(operationObject: OperationObject): any {
+        const status = Object.keys(operationObject.responses)[0];
+        const schema = operationObject.responses[status].content['application/json'].schema;
+
+        let response: any = {};
+
+        Object.keys(schema.properties).forEach(key => {
+            if (schema.required.includes(key)) {
+                switch(schema.properties[key].type) {
+                    case 'integer': {
+                        response[key] = 3;
+                        break;
+                    }
+                    case 'string': {
+                        response[key] = 'testString';
+                        break;
+                    }
+                }
+            }
+        });
+
+        return response;
     }
 
 }
--- src/spec/services/mock.service.spec.ts
+++ src/spec/services/mock.service.spec.ts
@@ -1,10 +1,77 @@
 import 'mocha';
-import {expect} from 'chai';
+import { expect } from 'chai';
+import { MockService } from '../../app/services/mock.service';
+import { OperationObject } from '../../app/models/operation-object';
 
 describe('MockService', () => {
 
-    it('true should be true', () => {
-        expect(true).to.be.true;
+    let mockService: MockService;
+
+    beforeEach(() => {
+        mockService = new MockService();
     })
 
-})
+    it('parseParamsInPath() should replace brackets params to params with colon', () => {
+        const path = '/users/{userId}/books/{bookId}';
+        const expectedPath = '/users/:userId/books/:bookId'
+        expect(mockService.parseParamsInPath(path)).to.equal(expectedPath);
+    });
+
+    it('prepareGetResponse() should return get response based on passed operation object', () => {
+        const getResponse = mockService.prepareGetResponse(operation);
+        expect(getResponse.id).to.not.be.undefined;
+        expect(getResponse.id).to.be.a('number');
+        expect(getResponse.name).to.not.be.undefined;
+        expect(getResponse.name).to.be.a('string');
+        if (getResponse.tag) {
+            expect(getResponse.tag).to.be.a('string');
+        }
+    })
+
+});
+
+const operation: OperationObject = {
+    "summary": "Info for a specific pet",
+    "operationId": "showPetById",
+    "tags": [
+        "pets"
+    ],
+    "parameters": [
+        {
+            "name": "petId",
+            "in": "path",
+            "required": true,
+            "description": "The id of the pet to retrieve",
+            "schema": {
+                "type": "string"
+            }
+        }
+    ],
+    "responses": {
+        "200": {
+            "description": "Expected response to a valid request",
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "required": [
+                            "id",
+                            "name"
+                        ],
+                        "properties": {
+                            "id": {
+                                "type": "integer",
+                                "format": "int64"
+                            },
+                            "name": {
+                                "type": "string"
+                            },
+                            "tag": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
